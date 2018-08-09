606a265b0c062bd1474b3fc6d8e8e45e60fb5b73
Ehabmagdyabbas <ihabmajdyabass@hotmail.com>
2018-08-09 17:42:05 +0200

    Add files via upload

---
+++ UTM to ETM/UTM to ETM/Form1.Designer.vb
@@ -0,0 +1,323 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.lon = New System.Windows.Forms.RadioButton()
+        Me.lat = New System.Windows.Forms.RadioButton()
+        Me.dec = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.a35 = New System.Windows.Forms.RadioButton()
+        Me.a31 = New System.Windows.Forms.RadioButton()
+        Me.a27 = New System.Windows.Forms.RadioButton()
+        Me.Eas = New System.Windows.Forms.Label()
+        Me.No = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.S2 = New System.Windows.Forms.TextBox()
+        Me.M2 = New System.Windows.Forms.TextBox()
+        Me.D2 = New System.Windows.Forms.TextBox()
+        Me.S1 = New System.Windows.Forms.TextBox()
+        Me.M1 = New System.Windows.Forms.TextBox()
+        Me.D1 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(310, 13)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(99, 82)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 56
+        Me.PictureBox1.TabStop = False
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(3, 105)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(136, 13)
+        Me.Label12.TabIndex = 55
+        Me.Label12.Text = "Checking : -------------------------"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(3, 92)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(136, 13)
+        Me.Label11.TabIndex = 54
+        Me.Label11.Text = "Checking : -------------------------"
+        '
+        'lon
+        '
+        Me.lon.AutoSize = True
+        Me.lon.Location = New System.Drawing.Point(49, 7)
+        Me.lon.Name = "lon"
+        Me.lon.Size = New System.Drawing.Size(45, 17)
+        Me.lon.TabIndex = 53
+        Me.lon.TabStop = True
+        Me.lon.Text = "long"
+        Me.lon.UseVisualStyleBackColor = True
+        '
+        'lat
+        '
+        Me.lat.AutoSize = True
+        Me.lat.Location = New System.Drawing.Point(6, 7)
+        Me.lat.Name = "lat"
+        Me.lat.Size = New System.Drawing.Size(36, 17)
+        Me.lat.TabIndex = 52
+        Me.lat.TabStop = True
+        Me.lat.Text = "lat"
+        Me.lat.UseVisualStyleBackColor = True
+        '
+        'dec
+        '
+        Me.dec.Location = New System.Drawing.Point(100, 4)
+        Me.dec.Name = "dec"
+        Me.dec.Size = New System.Drawing.Size(89, 20)
+        Me.dec.TabIndex = 51
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(195, 2)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(109, 23)
+        Me.Button3.TabIndex = 50
+        Me.Button3.Text = "Decmi to degree"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'a35
+        '
+        Me.a35.AutoSize = True
+        Me.a35.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.a35.Location = New System.Drawing.Point(213, 30)
+        Me.a35.Name = "a35"
+        Me.a35.Size = New System.Drawing.Size(90, 56)
+        Me.a35.TabIndex = 49
+        Me.a35.Text = "long = 35  " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "lat    = 30" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "N = 1100000 " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "E = 300000" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.a35.UseVisualStyleBackColor = False
+        '
+        'a31
+        '
+        Me.a31.AutoSize = True
+        Me.a31.BackColor = System.Drawing.Color.MistyRose
+        Me.a31.Checked = True
+        Me.a31.Location = New System.Drawing.Point(117, 30)
+        Me.a31.Name = "a31"
+        Me.a31.Size = New System.Drawing.Size(84, 56)
+        Me.a31.TabIndex = 48
+        Me.a31.TabStop = True
+        Me.a31.Text = "long = 31" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "lat    = 30" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "N = 810000 " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "E  = 615000" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.a31.UseVisualStyleBackColor = False
+        '
+        'a27
+        '
+        Me.a27.AutoSize = True
+        Me.a27.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.a27.Location = New System.Drawing.Point(5, 30)
+        Me.a27.Name = "a27"
+        Me.a27.Size = New System.Drawing.Size(95, 56)
+        Me.a27.TabIndex = 47
+        Me.a27.Text = "long = 27  " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "lat    = 30" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "N     = 200000" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "E      = 700000" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.a27.UseVisualStyleBackColor = False
+        '
+        'Eas
+        '
+        Me.Eas.AutoSize = True
+        Me.Eas.Location = New System.Drawing.Point(305, 149)
+        Me.Eas.Name = "Eas"
+        Me.Eas.Size = New System.Drawing.Size(14, 13)
+        Me.Eas.TabIndex = 46
+        Me.Eas.Text = "E"
+        '
+        'No
+        '
+        Me.No.AutoSize = True
+        Me.No.Location = New System.Drawing.Point(307, 124)
+        Me.No.Name = "No"
+        Me.No.Size = New System.Drawing.Size(15, 13)
+        Me.No.TabIndex = 45
+        Me.No.Text = "N"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(253, 149)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(48, 13)
+        Me.Label9.TabIndex = 44
+        Me.Label9.Text = "Easting :"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(252, 124)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(48, 13)
+        Me.Label10.TabIndex = 43
+        Me.Label10.Text = "North    :"
+        '
+        'S2
+        '
+        Me.S2.Location = New System.Drawing.Point(178, 146)
+        Me.S2.Name = "S2"
+        Me.S2.Size = New System.Drawing.Size(67, 20)
+        Me.S2.TabIndex = 42
+        Me.S2.Text = "0"
+        '
+        'M2
+        '
+        Me.M2.Location = New System.Drawing.Point(115, 146)
+        Me.M2.Name = "M2"
+        Me.M2.Size = New System.Drawing.Size(57, 20)
+        Me.M2.TabIndex = 41
+        Me.M2.Text = "0"
+        '
+        'D2
+        '
+        Me.D2.Location = New System.Drawing.Point(70, 146)
+        Me.D2.Name = "D2"
+        Me.D2.Size = New System.Drawing.Size(35, 20)
+        Me.D2.TabIndex = 40
+        Me.D2.Text = "29"
+        '
+        'S1
+        '
+        Me.S1.Location = New System.Drawing.Point(178, 121)
+        Me.S1.Name = "S1"
+        Me.S1.Size = New System.Drawing.Size(67, 20)
+        Me.S1.TabIndex = 39
+        Me.S1.Text = "0"
+        '
+        'M1
+        '
+        Me.M1.Location = New System.Drawing.Point(115, 121)
+        Me.M1.Name = "M1"
+        Me.M1.Size = New System.Drawing.Size(57, 20)
+        Me.M1.TabIndex = 38
+        Me.M1.Text = "0"
+        '
+        'D1
+        '
+        Me.D1.Location = New System.Drawing.Point(69, 121)
+        Me.D1.Name = "D1"
+        Me.D1.Size = New System.Drawing.Size(36, 20)
+        Me.D1.TabIndex = 37
+        Me.D1.Text = "27"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(2, 149)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(57, 13)
+        Me.Label8.TabIndex = 36
+        Me.Label8.Text = "Longtitude"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(2, 128)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(45, 13)
+        Me.Label7.TabIndex = 35
+        Me.Label7.Text = "Latitude"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(70, 172)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(180, 23)
+        Me.Button2.TabIndex = 34
+        Me.Button2.Text = "Geographic to ETM"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(410, 196)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.lon)
+        Me.Controls.Add(Me.lat)
+        Me.Controls.Add(Me.dec)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.a35)
+        Me.Controls.Add(Me.a31)
+        Me.Controls.Add(Me.a27)
+        Me.Controls.Add(Me.Eas)
+        Me.Controls.Add(Me.No)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.S2)
+        Me.Controls.Add(Me.M2)
+        Me.Controls.Add(Me.D2)
+        Me.Controls.Add(Me.S1)
+        Me.Controls.Add(Me.M1)
+        Me.Controls.Add(Me.D1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button2)
+        Me.Name = "Form1"
+        Me.Text = "Egyption Coordinates"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents lon As System.Windows.Forms.RadioButton
+    Friend WithEvents lat As System.Windows.Forms.RadioButton
+    Friend WithEvents dec As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents a35 As System.Windows.Forms.RadioButton
+    Friend WithEvents a31 As System.Windows.Forms.RadioButton
+    Friend WithEvents a27 As System.Windows.Forms.RadioButton
+    Friend WithEvents Eas As System.Windows.Forms.Label
+    Friend WithEvents No As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents S2 As System.Windows.Forms.TextBox
+    Friend WithEvents M2 As System.Windows.Forms.TextBox
+    Friend WithEvents D2 As System.Windows.Forms.TextBox
+    Friend WithEvents S1 As System.Windows.Forms.TextBox
+    Friend WithEvents M1 As System.Windows.Forms.TextBox
+    Friend WithEvents D1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ UTM to ETM/UTM to ETM/Form1.vb
@@ -0,0 +1,122 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        Dim D3 As Double
+        Dim X0, Y0 As Double
+        If a27.Checked = True Then D3 = 27 : X0 = 200000 : Y0 = 700000
+        If a31.Checked = True Then D3 = 31 : X0 = 810000 : Y0 = 615000
+        If a35.Checked = True Then D3 = 35 : X0 = 1100000 : Y0 = 300000
+
+
+        Dim PI As Double = 3.1415926535897931
+        Dim A1 As Double = 6378200 'دائرة نصف قطرها الاستوائية، م
+        'Name  ellipsoid            Equatorial radius, m
+        'Krasovskogo (1940)             6378245
+        'International(1924)            6378388 
+        'Clarke(1880)                   6378249 
+        'Bessel (1841)                  6377397 
+        'Erie (1830)                    6377563      
+        'Everest(1830)                  6377276            
+        'Helmert(1906)                  6378200 
+        'WGS66 (1966)                   6378145 
+        'GRS67 (1967)                   6378160
+        'WGS72 (1972)                   6378135 
+        'GRS80(1979)                    6378137
+        'WGS84                          6378137
+        Dim E2 As Double = 0.00669342162296594 'e^2 = 0.006693421623 > eccentricity  إختلاف المراكز
+        Dim A0 As Double = A1 * (1 - E2 / 4 - E2 ^ 2 * 3 / 64 - E2 ^ 3 * 5 / 256)
+        Dim B0 As Double = A1 * 3 / 8 * (E2 + E2 ^ 2 / 4 + E2 ^ 3 * 15 / 128)
+        Dim C0 As Double = A1 * 15 / 256 * (E2 ^ 2 + E2 ^ 3 * 3 / 4)
+        Dim D0 As Double = A1 * E2 ^ 3 * 35 / 3072
+
+        Dim N0 As Double = 30 * PI / 180
+        Dim S0 As Double = A0 * N0 - B0 * Math.Sin(2 * N0) + C0 * Math.Sin(4 * N0) - D0 * Math.Sin(6 * N0)
+
+
+        Dim X As Double = (D1.Text + M1.Text / 60 + S1.Text / 3600) * PI / 180
+        Dim Y As Double = (D2.Text + M2.Text / 60 + S2.Text / 3600) * PI / 180
+        Dim S As Double = A0 * X - B0 * Math.Sin(2 * X) + C0 * Math.Sin(4 * X) - D0 * Math.Sin(6 * X)
+        Dim P As Double = A1 * (1 - E2) / (1 - E2 * Math.Sin(X) ^ 2) ^ 1.5
+        Dim V As Double = A1 / (1 - E2 * Math.Sin(X) ^ 2) ^ 0.5
+        Dim C As Double = V / P
+        S = S - S0
+        Dim W As Double = Y - D3 * PI / 180
+        Dim T As Double = Math.Tan(X)
+        Dim O As Double = W * Math.Cos(X)
+        Dim N As Double
+        N = S + V * Math.Sin(X) * W * O / 2 + V * Math.Sin(X) * W * O ^ 3 / 24 * (4 * C ^ 2 + C - T ^ 2)
+        N = N + V * Math.Sin(X) * W * O ^ 5 / 720 * (8 * C ^ 4 * (11 - 24 * T ^ 2) - 28 * C ^ 3 * (1 - 6 * T ^ 2))
+        N = N + V * Math.Sin(X) * W * O ^ 5 / 720 * (C ^ 2 * (1 - 32 * T ^ 2) - C * 2 * T ^ 2 + T ^ 4)
+        N = N + V * Math.Sin(X) * W * O ^ 7 / 40320.0! * (1385 - 3111 * T ^ 2 + 543 * T ^ 4 - T ^ 6)
+        N = X0 + N
+
+        Dim Ea As Double
+        Ea = V * O + V * O ^ 3 / 6 * (C - T ^ 2)
+        Ea = Ea + V * O ^ 5 / 120 * (4 * C ^ 3 * (1 - 6 * T ^ 2) + C ^ 2 * (1 + 8 * T ^ 2) - C ^ 2 * T ^ 2 + T ^ 4)
+        Ea = Ea + V * O ^ 7 / 5040 * (61 - 479 * T ^ 2 + 179 * T ^ 4 - T ^ 6)
+        Ea = Ea + Y0
+
+        If D1.Text < 22 Or D1.Text > 32 Then
+            Label11.Text = "Checking : --Latitude value  ERROR Outside Range --"
+            Label11.BackColor = Color.Red
+        Else
+            Label11.Text = "Checking : --Latitude value  Inside Range --"
+            Label11.BackColor = Color.LightGreen
+            No.Text = N
+        End If
+
+
+        If D2.Text < 24 Or D2.Text > 37 Then
+            Label12.Text = "Checking : --Longitude value ERROR Outside Range --"
+            Label12.BackColor = Color.Red
+        Else
+            Label12.Text = "Checking : --Longitude value  Inside Range --"
+            Label12.BackColor = Color.LightGreen
+            Eas.Text = Ea
+        End If
+
+
+
+
+    End Sub
+    Function Convert_Degree(ByVal Decimal_Deg) As Object
+        'source: http://support.microsoft.com/kb/213449
+        '
+        'converts a decimal degree representation to deg min sec
+        'as 10.46 returns 10° 27' 36"
+        '
+        Dim degrees As Object
+        Dim minutes As Object
+        Dim seconds As Object
+        ' With Application
+        'Set degree to Integer of Argument Passed
+        degrees = Int(Decimal_Deg)
+        'Set minutes to 60 times the number to the right
+        'of the decimal for the variable Decimal_Deg
+        minutes = (Decimal_Deg - degrees) * 60
+        'Set seconds to 60 times the number to the right of the
+        'decimal for the variable Minute
+        seconds = Format(((minutes - Int(minutes)) * 60), "0")
+        'Returns the Result of degree conversion
+        '(for example, 10.46 = 10° 27' 36")
+        Convert_Degree = " " & degrees & "° " & Int(minutes) & "' " _
+            & seconds + Chr(34)
+        ' End With
+
+        If lat.Checked = True Then
+            D1.Text = degrees
+            M1.Text = Int(minutes)
+            S1.Text = seconds
+
+        End If
+        If lon.Checked = True Then
+            D2.Text = degrees
+            M2.Text = Int(minutes)
+            S2.Text = seconds
+        End If
+    End Function
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Convert_Degree(dec.Text)
+    End Sub
+
+End Class
